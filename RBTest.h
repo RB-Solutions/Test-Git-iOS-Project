3c26af20fcd33f60d23312ace559397a0cc31c24
BobIntel2 <BobG5@Bobs-Intel.local>
2014-11-27 14:19:01 -0800

    added a test class

---
+++ RBTest.h
@@ -0,0 +1,13 @@
+//
+//  RBTest.h
+//  Git Test
+//
+//  Created by BobIntel2 on 11/27/2014.
+//  Copyright (c) 2014 BobIntel2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RBTest : NSObject
+
+@end
---
+++ RBTest.m
@@ -0,0 +1,13 @@
+//
+//  RBTest.m
+//  Git Test
+//
+//  Created by BobIntel2 on 11/27/2014.
+//  Copyright (c) 2014 BobIntel2. All rights reserved.
+//
+
+#import "RBTest.h"
+
+@implementation RBTest
+
+@end
